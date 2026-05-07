6e87725a85ce9fca554b210635a279290d6066ba
Manford <zengchang389@gmail.com>
2026-05-07 15:00:02 +1200

    test(product): cover Product.UpdatePrice invariants

--- tests/SecondHandShop.Domain.UnitTests/Entities/ProductTests.cs
+++ tests/SecondHandShop.Domain.UnitTests/Entities/ProductTests.cs
@@ -71,6 +71,64 @@ public class ProductTests
             .WithMessage("Cannot take a sold product off the shelf*");
     }
 
+    [Fact]
+    public void UpdatePrice_ShouldSetNewPriceAndTouchAudit_WhenAvailable()
+    {
+        var product = CreateProduct();
+        var admin = Guid.NewGuid();
+        var later = UtcNow.AddMinutes(5);
+
+        product.UpdatePrice(259m, admin, later);
+
+        product.Price.Should().Be(259m);
+        product.UpdatedAt.Should().Be(later);
+        product.UpdatedByAdminUserId.Should().Be(admin);
+    }
+
+    [Fact]
+    public void UpdatePrice_ShouldAllowUpdate_WhenOffShelf()
+    {
+        var product = CreateProduct();
+        product.OffShelf(Guid.NewGuid(), UtcNow.AddMinutes(1));
+
+        product.UpdatePrice(199m, Guid.NewGuid(), UtcNow.AddMinutes(2));
+
+        product.Price.Should().Be(199m);
+    }
+
+    [Fact]
+    public void UpdatePrice_ShouldThrow_WhenSold()
+    {
+        var product = CreateProduct();
+        _ = product.MarkAsSold(200m, UtcNow.AddMinutes(1), Guid.NewGuid(), UtcNow.AddMinutes(1));
+
+        var act = () => product.UpdatePrice(180m, Guid.NewGuid(), UtcNow.AddMinutes(2));
+
+        act.Should().Throw<DomainRuleViolationException>()
+            .WithMessage("Cannot modify price of a sold product. Revert the sale first.");
+    }
+
+    [Fact]
+    public void UpdatePrice_ShouldThrow_WhenNonPositive()
+    {
+        var product = CreateProduct();
+
+        var act = () => product.UpdatePrice(0m, Guid.NewGuid(), UtcNow.AddMinutes(1));
+
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void UpdatePrice_ShouldBeNoop_WhenPriceUnchanged()
+    {
+        var product = CreateProduct();
+        var originalUpdatedAt = product.UpdatedAt;
+
+        product.UpdatePrice(product.Price, Guid.NewGuid(), UtcNow.AddMinutes(10));
+
+        product.UpdatedAt.Should().Be(originalUpdatedAt);
+    }
+
     private static Product CreateProduct()
     {
         return Product.Create(
